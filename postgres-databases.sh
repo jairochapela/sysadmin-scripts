e548d1ba8f170c9e0c8dba4aebe054a55385a739
Jairo Chapela-Martinez <jairochapela@gmail.com>
2021-09-16 17:50:26 +0200

    First commit

---
+++ postgres-databases.sh
@@ -0,0 +1,163 @@
+#!/bin/sh
+
+
+ADMIN_USER=postgres
+
+
+ANSI_RESET="\033[0m"
+ANSI_BOLD="\033[1m"
+ANSI_RED="\033[31m"
+ANSI_GREEN="\033[32m"
+ANSI_YELLOW="\033[33m"
+
+heading() {
+    clear
+    echo $ANSI_GREEN $ANSI_BOLD
+    figlet $* || echo $*
+    echo $ANSI_RESET
+}
+
+message() {
+    echo $ANSI_YELLOW
+    cowsay $* || echo $*
+    echo $ANSI_RESET
+}
+
+error() {
+    echo $ANSI_RED
+    cowsay -e xx $* || echo "ERROR: $*"
+    echo $ANSI_RESET
+    return 1
+}
+
+
+choose_database_and_do() {
+    echo $ANSI_BOLD "Seleccionar base de datos:" $ANSI_RESET
+    psql -l -A -t|cut -d'|' -f1|nl
+    read -p "Opción> " n
+
+    test "$n" -gt 0 || return 1
+    bd=$(psql -l -A -t|cut -d'|' -f1|head -n $n|tail -n 1)
+    test -n "$bd" || return 2
+
+    eval "$* $bd"
+}
+
+
+ask_new_database_name_and_do() {
+    read -p "Nombre de la nueva base de datos: " bd && test -n "$bd" || return -1
+    eval "$* $bd"
+}
+
+
+_create_database() {
+    newdbname="$1"
+    echo "Creando base de datos $newdbname..."
+    sudo -u $ADMIN_USER psql -c "CREATE DATABASE $newdbname;" && \
+        message "Base de datos $newdbname creada con éxito" || \
+        error "Error creando la base de datos $newdbname"
+}
+
+
+new_database() {
+    ask_new_database_name_and_do _create_database
+}
+
+
+grant_user() {
+    dbname="$1"
+    defaultuser=$(whoami)
+    read -p "Usuario a autorizar [$defaultuser]: " username && test -n "$username" || username=$defaultuser
+    sudo -u $ADMIN_USER psql -c "GRANT ALL PRIVILEGES ON DATABASE \"$dbname\" TO \"$username\""  && \
+        message "Privilegios concedidos al usuario $username sobre la base de datos $dbname" || \
+        error "Error otorgando privilegios"
+}
+
+_dup_database() {
+    dbname="$1"
+    newdbname="$2"
+    _create_database "$newdbname" && \
+        (pg_dump "$dbname" | sudo -u $ADMIN_USER psql "$newdbname") && \
+        message "Base de datos $dbname duplicada en $newdbname"
+}
+
+duplicate_database() {
+    dbname="$1"
+    ask_new_database_name_and_do _dup_database $dbname
+}
+
+
+dump_database() {
+    dbname="$1"
+    defaultfname="${dbname}_`date +%Y-%m-%d_%H%M`.sql"
+    read -p "Nombre del fichero de salida [$defaultfname]: " sqlfile && test -n "$sqlfile" || sqlfile="$defaultfname"
+    pg_dump $dbname > $sqlfile && \
+        message "Base de datos $dbname volcada en $sqlfile" && stat $sqlfile || \
+        error "Error volcando la base de datos $dbname"
+}
+
+remove_database() {
+    dbname="$1"
+    message "Se va a eliminar la base de datos $dbname. ¿Estás completamente seguro?"
+    read -p "Escribe $dbname para confirmar la operación: " confirm && test "$dbname" = "$confirm" || return 1
+    sudo -u $ADMIN_USER psql -c "DROP DATABASE $dbname" && \
+        message "Base de datos $dbname eliminada con éxito" || \
+        error "Error eliminando la base de datos $dbname"
+}
+
+main_menu() {
+    while true ; do
+        heading "Postgres Databases"
+
+        echo $ANSI_BOLD"\nSelecciona una opción:\n"$ANSI_RESET
+        echo "new\t- Crear base de datos"
+        echo "g\t- Autorizar usuario"
+        echo "sql\t- Shell SQL"
+        echo "dup\t- Duplicar base de datos"
+        echo "bk\t- Volcado SQL de base de datos"
+        echo "rm\t- Eliminar base de datos"
+        echo "q\t- Salir"
+        echo ""
+        read -p "Comando> " c
+        case "$c" in
+            new)
+            heading "Crear base de datos"
+            new_database
+            ;;
+            g)
+            heading "Autorizar usuario"
+            choose_database_and_do grant_user
+            ;;
+            sql)
+            heading "Shell SQL"
+            choose_database_and_do psql
+            ;;
+            dup)
+            heading "Duplicar base de datos"
+            choose_database_and_do duplicate_database
+            ;;
+            bk)
+            heading "Volcado de base de datos"
+            choose_database_and_do dump_database
+            ;;
+            rm)
+            heading "Eliminar base de datos"
+            choose_database_and_do remove_database
+            ;;
+            q)
+            message "Bye!"
+            exit 0
+            ;;
+            *)
+            [ -z "$c" ] && message "Por favor, indica un comando." || error "Comando no reconocido."
+            ;;
+        esac
+
+        #[ "$?" = 0 ] || error "Ocurrió un error."
+
+        read -p "Pulsa intro para continuar." _k
+    done
+}
+
+
+main_menu 
