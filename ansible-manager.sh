22fb8233a0b3ff28e50f64f7db776eea679cc3c4
Jairo Chapela-Martinez <jairochapela@gmail.com>
2021-09-21 18:11:54 +0200

    Ansible manager

---
+++ ansible-manager.sh
@@ -0,0 +1,217 @@
+#!/bin/bash
+
+
+[ -n "$DATADIR" ] || DATADIR="$HOME/AnsiblePlaybooks"
+[ -n "$EDITOR" ] || EDITOR=vim
+
+
+INVENTORIES="testing staging production"
+
+
+ANSI_RESET="\033[0m"
+ANSI_BOLD="\033[1m"
+ANSI_RED="\033[31m"
+ANSI_GREEN="\033[32m"
+ANSI_YELLOW="\033[33m"
+
+
+VAULT_PASSWD_FILE="$HOME/.ansible/vault_password"
+
+
+
+setup() {
+    mkdir -p $DATADIR
+    mkdir -p $DATADIR/roles
+    mkdir -p $DATADIR/inventories
+    mkdir -p $DATADIR/inventories/host_vars
+    mkdir -p $DATADIR/inventories/group_vars
+}
+
+
+check_vault_password_file() {
+    [ -f $VAULT_PASSWD_FILE ] && return
+    read -s -p "Vault password: " vault_password
+    echo -n "$vault_password" > $VAULT_PASSWD_FILE
+    chmod 600 $VAULT_PASSWD_FILE    
+}
+
+title() {
+    echo -e $ANSI_GREEN $ANSI_BOLD
+    command -v figlet >/dev/null && figlet -w `tput cols` $* || echo $*
+    echo -e $ANSI_RESET
+}
+
+
+heading() {
+    echo -e $ANSI_BOLD$*$ANSI_RESET
+}
+
+show_error() {
+    echo -e $ANSI_RED"Error: "$*$ANSI_RESET
+}
+
+show_message() {
+    echo -e $ANSI_YELLOW$*$ANSI_RESET
+}
+
+edit_file() {
+    $EDITOR $*
+    return $?
+}
+
+
+edit_vault_file() {
+    vaultfile=$1
+    check_vault_password_file
+    [ -f $vaultfile ] && ansible-vault --vault-password-file $VAULT_PASSWD_FILE edit $vaultfile || ansible-vault --vault-password-file $VAULT_PASSWD_FILE create $vaultfile
+    return $?
+}
+
+
+choose_inventory_then() {
+    heading "Selecciona un entorno"
+    PS3="Entorno (0 para cancelar)> "
+    select inventory in $INVENTORIES ; do
+        [ $REPLY == 0 ] && return
+        $* $DATADIR/inventories/$inventory.ini
+        [ $? == 0 ] && return
+        REPLY=''
+    done
+}
+
+
+choose_playbook_then() {
+    heading "Selecciona un playbook"
+    playbooks=$(find "$DATADIR" -maxdepth 1 -path '*.yml') || return 1
+    PS3="Playbook (0 para cancelar)> "
+    select playbook in $playbooks ; do
+        [ $REPLY == 0 ] && return
+        $* $playbook
+        [ $? == 0 ] && return
+    done
+}
+
+
+run_playbook() {
+    playbook=$1
+    inventory=$2
+    check_vault_password_file
+    ansible-playbook --vault-password-file $VAULT_PASSWD_FILE -i $inventory $playbook
+}
+
+
+ask_new_filename_then() {
+    prompt=$1
+    prefix=$2
+    suffix=$3
+    shift
+    shift
+    shift
+    while true ; do
+        read -p "Nombre del nuevo playbook: " fname
+        [ ! -f $prefix$fname$suffix ] && break
+    done
+    $* $prefix$fname$suffix
+}
+
+
+new_playbook() {
+    playbookfile="$1"
+    read -p "Descripción: " description
+    read -p "Grupo de hosts: " grphosts  
+echo "
+---
+  - name: $description
+    hosts: $grphosts
+    become: yes
+    become_user: root
+    roles:
+      - rol1
+      - rol2
+    tasks:
+      - name: sample task
+        debug:
+          msg: To be done
+" > "$playbookfile"
+    edit_file "$playbookfile"
+}
+
+
+edit_inventory() {
+    choose_inventory_then edit_file
+}
+
+
+edit_host_vars() {
+    inventory=$1
+    #check_vault_password_file
+    heading "Selecciona un host"
+    #hosts=$(ansible-inventory --list -i $inventory --vault-password-file $VAULT_PASSWD_FILE|jq -r '.[]|select(has("hosts"))|.[][]')
+    hosts=$(cat $inventory|grep -v '\['|cut -d' ' -f1|sort|uniq)
+    PS3="Host (0 para cancelar)> "
+    select host in $hosts ; do
+        [ $REPLY == 0 ] && return
+        check_vault_password_file
+        edit_vault_file $DATADIR/inventories/host_vars/$host.yml
+    done
+}
+
+edit_group_vars() {
+    inventory=$1
+    #check_vault_password_file
+    heading "Selecciona un grupo"
+    #groups=$(ansible-inventory --list -i $inventory --vault-password-file $VAULT_PASSWD_FILE|jq -r '.all.children[]'|grep -v 'ungrouped')
+    #$(cat $inventory|grep '^\[' |cut -f2 -d'['|cut -f1 -d']')
+    groups=$(cat $inventory|sed -n 's/^[ \t]*\[\(.*\)\].*/\1/p'|sort|uniq)
+    PS3="Grupo (0 para cancelar)> "
+    select group in $groups ; do
+        [ $REPLY == 0 ] && return
+        check_vault_password_file
+        edit_vault_file $DATADIR/inventories/group_vars/$group.yml
+    done
+}
+
+
+main_menu() {
+    while true ; do
+        clear
+        title "Ansible Manager"
+        heading "Selecciona una opción"
+        echo -e "p\t- Ejecutar playbook"
+        echo -e "n\t- Crear nuevo playbook"
+        echo -e "e\t- Editar playbook"
+        echo -e "k\t- Eliminar playbook"
+        echo -e "i\t- Inventario"
+        echo -e "h\t- Variables de host"
+        echo -e "g\t- Variables de grupo de hosts"
+        #echo -e "s\t- Copiar clave SSH"
+        echo -e "0\t- Salir"
+        read -p "Opción> " op
+
+        case $op in
+        p) choose_playbook_then choose_inventory_then run_playbook ;;
+        n) ask_new_filename_then "Nombre del nuevo playbook: " "$DATADIR/" ".yml" new_playbook ;;
+        e) choose_playbook_then edit_file ;;
+        k) choose_playbook_then rm -i ;;
+        i) choose_inventory_then edit_file ;;
+        h) choose_inventory_then edit_host_vars ;;
+        g) choose_inventory_then edit_group_vars ;;
+        0) return ;;
+        *) [ -z "$c" ] && show_message "Por favor, indica un comando." || show_error "Comando no reconocido." ;;
+        esac
+
+        [ "$?" == 0 ] || (show_error "Ocurrió un error." )
+
+        read -p "Pulse intro para continuar." _k
+        
+
+    done    
+}
+
+
+
+
+setup
+main_menu
+
+rm -f $VAULT_PASSWD_FILE
--- postgres-databases.sh
+++ postgres-databases.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 
 ADMIN_USER=postgres
@@ -12,35 +12,36 @@ ANSI_YELLOW="\033[33m"
 
 heading() {
     clear
-    echo $ANSI_GREEN $ANSI_BOLD
+    echo -e $ANSI_GREEN $ANSI_BOLD
     command -v figlet >/dev/null && figlet -w `tput cols` $* || echo $*
-    echo $ANSI_RESET
+    echo -e $ANSI_RESET
 }
 
 message() {
-    echo $ANSI_YELLOW
+    echo -e $ANSI_YELLOW
     command -v cowsay >/dev/null && cowsay $* || echo $*
-    echo $ANSI_RESET
+    echo -e $ANSI_RESET
 }
 
 error() {
-    echo $ANSI_RED
+    echo -e $ANSI_RED
     command -v cowsay >/dev/null && cowsay -e xx $* || echo "ERROR: $*"
-    echo $ANSI_RESET
+    echo -e $ANSI_RESET
     return 1
 }
 
 
 choose_database_and_do() {
-    echo $ANSI_BOLD "Seleccionar base de datos:" $ANSI_RESET
-    sudo -u $ADMIN_USER psql -l -A -t|cut -d'|' -f1|nl
-    read -p "Opción> " n
+    echo -e $ANSI_BOLD "Seleccionar base de datos (0 para cancelar):" $ANSI_RESET
+    select bd in $(sudo -u $ADMIN_USER psql -l -A -t|cut -d'|' -f1) ; do
+        test -n "$bd" || return 2
+        eval "$* $bd"
+        return $?
+    done
+#    read -p "Opción> " n
 
-    test "$n" -gt 0 || return 1
-    bd=$(sudo -u $ADMIN_USER psql -l -A -t|cut -d'|' -f1|head -n $n|tail -n 1)
-    test -n "$bd" || return 2
-
-    eval "$* $bd"
+#    test "$n" -gt 0 || return 1
+#    bd=$(sudo -u $ADMIN_USER psql -l -A -t|cut -d'|' -f1|head -n $n|tail -n 1)
 }
 
 
@@ -94,7 +95,7 @@ _dup_database() {
     dbname="$1"
     newdbname="$2"
     _create_database "$newdbname" && \
-        (pg_dump "$dbname" | sudo -u $ADMIN_USER psql "$newdbname") && \
+        (sudo -u $ADMIN_USER pg_dump "$dbname" | sudo -u $ADMIN_USER psql "$newdbname") && \
         message "Base de datos $dbname duplicada en $newdbname"
 }
 
@@ -113,6 +114,16 @@ dump_database() {
         error "Error volcando la base de datos $dbname"
 }
 
+
+restore_database() {
+    dbname="$1"
+    read -e -p "Nombre del fichero de entrada: " sqlfile
+    test -f "$sqlfile" || error "No existe ese fichero"
+    sudo -u $ADMIN_USER psql "$dbname" < "$sqlfile" && \
+        message "Base de datos $dbname restaurada a partir de $sqlfile" || \
+        error "No se pudo restaurar la base de datos $dbname"
+}
+
 remove_database() {
     dbname="$1"
     message "Se va a eliminar la base de datos $dbname. ¿Estás completamente seguro?"
@@ -126,35 +137,61 @@ main_menu() {
     while true ; do
         heading "Postgres Databases"
 
-        echo $ANSI_BOLD"\nSelecciona una opción:\n"$ANSI_RESET
-        echo "new\t- Crear base de datos"
-	echo "user\t- Crear usuario"
-        echo "grant\t- Autorizar usuario"
-	echo "ls\t- Listar bases de datos"
-        echo "sql\t- Shell SQL"
-        echo "dup\t- Duplicar base de datos"
-        echo "bk\t- Volcado SQL de base de datos"
-        echo "rm\t- Eliminar base de datos"
-        echo "q\t- Salir"
-        echo ""
+        echo -e "$ANSI_BOLD\nSelecciona una opción:\n$ANSI_RESET"
+
+#        select c in \
+#        "Crear base de datos" \
+#        "Crear usuario" \
+#        "Autorizar usuario" \
+#        "Listado de bases de datos" \
+#        "Shell SQL" \
+#        "Duplicar base de datos" \
+#        "Volcado SQL de base de datos" \
+#        "Eliminar base de datos" ; do
+#
+#            heading $c
+#            case $REPLY in
+#            1) new_database ;;
+#            2) new_user ;;
+#            3) choose_database_and_do grant_user ;;
+#            4) ls_databases ;;
+#            5) choose_database_and_do sql_shell ;;
+#            6) choose_database_and_do duplicate_database ;;
+#            7) choose_database_and_do dump_database ;;
+#            8) choose_database_and_do restore_database ;;
+#            9) choose_database_and_do remove_database ;;
+#            esac
+#        done
+
+        echo -e "new\t- Crear base de datos"
+        echo -e "user\t- Crear usuario"
+        echo -e "grant\t- Autorizar usuario"
+        echo -e "ls\t- Listar bases de datos"
+        echo -e "sql\t- Shell SQL"
+        echo -e "dup\t- Duplicar base de datos"
+        echo -e "bk\t- Volcado SQL de base de datos"
+        echo -e "rm\t- Eliminar base de datos"
+        echo -e "rst\t- Restaurar base de datos"
+        echo -e "q\t- Salir"
+
         read -p "Comando> " c
         case "$c" in
             new)
             heading "Crear base de datos"
             new_database
             ;;
-	    user)
-	    heading "Crear usuario"
-	    new_user
+	        user)
+	        heading "Crear usuario"
+	        new_user
             ;;
             grant)
             heading "Autorizar usuario"
             choose_database_and_do grant_user
             ;;
-	    ls)
-	    heading "Bases de datos"
-	    ls_databases
-	    ;;
+	        ls)
+	        heading "Bases de datos"
+	        ls_databases
+	        ;;
             sql)
             heading "Shell SQL"
             choose_database_and_do sql_shell
@@ -166,6 +203,10 @@ main_menu() {
             bk)
             heading "Volcado de base de datos"
             choose_database_and_do dump_database
+            ;;
+	        rst)
+	        heading "Restaurar base de datos"
+	        choose_database_and_do restore_database
             ;;
             rm)
             heading "Eliminar base de datos"
