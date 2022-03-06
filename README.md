8a77531c40edcd031818c12e9b9cb444ec36f825
Jairo Chapela-Martinez <jairochapela@gmail.com>
2022-03-06 18:33:49 +0100

    Ajustes y documentación

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Scripts de administración de sistemas
+
+
+
+## Scripts de backup
+
+En `backup-borg.sh` debe configurarse el repositorio y la clave de cifrado en el propio script.
+
+### Configuración de cron
+
+```
+10 1 * * * backup-postgres.sh postgres bd1 bd2
+30 3 * * * backup-borg.sh /home /etc/apache2 /srv/storage /var/backup/postgres
+```
+
--- backup-borg.sh
+++ backup-borg.sh
@@ -5,7 +5,7 @@ hostname=$(hostname)
 export BORG_REPO="ssh://uuuuuu@backup-server.xyz:23/~/$hostname.borg"
 export BORG_PASSPHRASE='xxxxxxxxxxxxxxxxxxxxxxxxxx'
 
-borg create --stats -C lz4 --exclude-caches ::`date +%Y%m%d%H%M` /home /var/backup/postgres
+borg create --stats -C lz4 --exclude-caches ::`date +%Y%m%d%H%M` $*
 borg prune --list --show-rc --keep-daily 7 --keep-weekly 4 --keep-monthly 6
 #borg compact
 
