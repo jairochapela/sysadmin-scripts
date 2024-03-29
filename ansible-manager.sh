7efc8d7c174c16e4418e0105a1d17bce2eacb797
Jairo Chapela-Martínez <jairo@Mac-mini-de-Jairo.local>
2024-03-29 19:39:34 +0100

    Reformulación de todo Ansible Manager

--- ansible-manager.sh
+++ ansible-manager.sh
@@ -12,7 +12,7 @@ ANSI_GREEN="\033[32m"
 ANSI_YELLOW="\033[33m"
 
 
-VAULT_PASSWD_FILE="$HOME/.ansible/vault_password"
+VAULT_PASSWD_FILE="$HOME/.ansible/vault_password_$(openssl rand -hex 4)"
 
 
 
@@ -46,6 +46,10 @@ title() {
 }
 
 
+subtitle() {
+    echo -e "$ANSI_BOLD$ANSI_YELLOW--- $* ---$ANSI_RESET\n"
+}
+
 heading() {
     echo -e $ANSI_BOLD$*$ANSI_RESET
 }
@@ -57,6 +61,13 @@ show_error() {
 show_message() {
     echo -e $ANSI_YELLOW$*$ANSI_RESET
 }
+
+
+confirm_and_do() {
+    read -p "¿Estás seguro? [S/N] " yn
+    [[ "$yn" == [Ss]* ]] && $* || return 1
+}
+
 
 edit_file() {
     $EDITOR $*
@@ -83,7 +94,7 @@ choose_inventory_then() {
     heading "Selecciona un entorno"
     PS3="Entorno (0 para cancelar)> "
     select inventory in $INVENTORIES ; do
-        [ $REPLY == 0 ] && return
+        [ $REPLY == 0 ] && return -1
         $* $CMDB_ANSIBLE_PATH/inventories/$inventory
         [ $? == 0 ] && return
         REPLY=''
@@ -104,10 +115,13 @@ choose_playbook_then() {
 
 
 run_playbook() {
-    playbook=$1
-    inventory=$2
+    inventory=$1
+    playbook=$2
+    shift 2
     check_vault_password_file
     ANSIBLE_VAULT_PASSWORD_FILE="$VAULT_PASSWD_FILE" ansible-playbook -i $inventory $playbook
+    read -p "Pulse intro para continuar." _k
+    return $?
 }
 
 
@@ -115,9 +129,7 @@ ask_new_filename_then() {
     prompt=$1
     prefix=$2
     suffix=$3
-    shift
-    shift
-    shift
+    shift 3
     while true ; do
         read -p "Nombre del nuevo playbook: " fname
         [ ! -f $prefix$fname$suffix ] && break
@@ -126,18 +138,37 @@ ask_new_filename_then() {
 }
 
 
+choose_role_then() {
+    heading "Selecciona un rol"
+    roles=$(find "$CMDB_ANSIBLE_PATH/roles" -depth 1 -path '*' -type d |xargs -I{} basename {}) || return 1
+    PS3="Rol (0 para cancelar)> "
+    select role in $roles ; do
+        [ $REPLY == 0 ] && return
+        $* "$CMDB_ANSIBLE_PATH/roles/$role"
+        [ $? == 0 ] && return
+    done
+}
+
+
 new_playbook() {
     playbookfile="$1"
     read -p "Descripción: " description
     read -p "Grupo de hosts: " grphosts
-    roles=$(find "$CMDB_ANSIBLE_PATH/roles" -depth 1 -path '*' -type d |xargs basename) || return 1
+    roles=$(find "$CMDB_ANSIBLE_PATH/roles" -depth 1 -path '*' -type d |xargs -I{} basename {}) || return 1
     PS3="Incorporar rol (0 para terminar)> "
+    selected_roles=""
     roles_to_add=""
     select role in $roles ; do
         [ $REPLY == 0 ] && break
+        [ -z "$role" ] && continue
+        selected_roles="$selected_roles $role"
         roles_to_add="$roles_to_add\n      - $role"
         echo -e "Roles seleccionados: $roles_to_add"
+
     done
+    #variables_to_add=$(for r in $selected_roles ; do find "$CMDB_ANSIBLE_PATH/roles/$r/defaults" |xargs cat|sed -e 's/^/        /' ; done)
+    variables_to_add=$(for r in $selected_roles ; do test -f "$CMDB_ANSIBLE_PATH/roles/$r/defaults/main.yml" && cat "$CMDB_ANSIBLE_PATH/roles/$r/defaults/main.yml"|sed -e 's/^/        /'; done)
+    #read
 echo -e "
 ---
   - name: $description
@@ -145,6 +176,8 @@ echo -e "
     become: yes
     become_user: root
     roles:$roles_to_add
+    vars:
+$variables_to_add
     tasks:
       - name: sample task
         debug:
@@ -165,6 +198,7 @@ edit_host_vars() {
     select host in $hosts ; do
         [ $REPLY == 0 ] && return
         edit_vault_file $inventory/host_vars/$host.yml
+        [ $? == 0 ] && return
     done
 }
 
@@ -177,7 +211,7 @@ remote_shell() {
     hosts=$(cat $inventory|grep -v '\['|cut -d' ' -f1|sort|uniq)
     PS3="Host (0 para cancelar)> "
     select host in $hosts ; do
-        [ $REPLY == 0 ] && return
+        [ $? == 0 ] && return
         ssh $host
     done
 }
@@ -195,6 +229,7 @@ edit_group_vars() {
         [ $REPLY == 0 ] && return
         check_vault_password_file
         edit_vault_file $inventory/group_vars/$group.yml
+        [ $REPLY == 0 ] && return
     done
 }
 
@@ -254,43 +289,185 @@ encrypt_vault_file() {
 }
 
 
-main_menu() {
+
+playbook_actions_menu() {
     while true ; do
         clear
-        title "Ansible Manager"
+        title "Gestionar playbooks"
         heading "Selecciona una opción"
-        echo -e "p\t- Ejecutar playbook"
         echo -e "n\t- Crear nuevo playbook"
         echo -e "e\t- Editar playbook"
         echo -e "k\t- Eliminar playbook"
-        echo -e "i\t- Inventario"
-        echo -e "h\t- Variables de host"
-        echo -e "g\t- Variables de grupo de hosts"
-        echo -e "s\t- Shell remota"
-    	echo -e "r\t- Gestionar roles"
-        echo -e "v\t- Cifrar fichero con Ansible Vault"
-        #echo -e "s\t- Copiar clave SSH"
-        echo -e "0\t- Salir"
+        #echo -e "r\t- Ejecutar playbook"
+        echo -e "0\t- Volver"
+
         read -p "Opción> " op
 
         case $op in
-        p) choose_playbook_then choose_inventory_then run_playbook ;;
         n) ask_new_filename_then "Nombre del nuevo playbook: " "$CMDB_ANSIBLE_PATH/" ".yml" new_playbook ;;
         e) choose_playbook_then edit_file ;;
         k) choose_playbook_then rm -i ;;
-        i) choose_inventory_then edit_inventory ;;
-        h) choose_inventory_then edit_host_vars ;;
-        g) choose_inventory_then edit_group_vars ;;
-        s) choose_inventory_then remote_shell ;;
-	    r) manage_roles ;;
-        v) choose_file_then encrypt_vault_file ;;
+        #r) choose_inventory_then run_playbook -i $inventory -p ;;        
         0) return ;;
         *) [ -z "$c" ] && show_message "Por favor, indica un comando." || show_error "Comando no reconocido." ;;
         esac
 
         [ "$?" == 0 ] || (show_error "Ocurrió un error." )
 
-        read -p "Pulse intro para continuar." _k
+        #read -p "Pulse intro para continuar." _k        
+    done    
+}
+
+
+choose_file_from_and_do() {
+    dir="$1"
+    shift
+    heading "Selecciona un fichero"
+    PS3="Fichero (0 para cancelar)> "
+    select file in $dir/* ; do
+        [ $REPLY == 0 ] && return
+        $* $file
+        [ $? == 0 ] && return
+    done
+}
+
+
+edit_role_actions_menu() {
+    role=$1
+    while true ; do
+        clear
+        title "Editar rol"
+        subtitle "Rol: $(basename $role)"
+        heading "Selecciona una opción"
+        echo -e "t\t- Editar tareas"
+        echo -e "j\t- Crear plantilla"
+        echo -e "p\t- Editar plantilla"
+        echo -e "v\t- Editar variables por defecto"
+        #echo -e "k\t- Eliminar fichero"
+        #echo -e "n\t- Crear nuevo fichero de tareas"
+        echo -e "0\t- Volver"
+
+        read -p "Opción> " op
+
+        case $op in
+        t) edit_file "$role/tasks/main.yml" ;;
+        v) edit_file "$role/defaults/main.yml" ;;
+        j) ask_new_filename_then "Nombre de la nueva plantilla: " "$role/templates" ".j2" edit_file ;;
+        p) choose_file_from_and_do "$role/templates" edit_file ;;
+        #k) confirm_and_do rm -rf "$role" ;;
+        0) return ;;
+        *) [ -z "$c" ] && show_message "Por favor, indica un comando." || show_error "Comando no reconocido." ;;
+        esac
+
+        [ "$?" == 0 ] || (show_error "Ocurrió un error." )
+
+        #read -p "Pulse intro para continuar." _k        
+    done        
+}
+
+
+new_role() {
+    role=$1
+    mkdir -p $role/{tasks,handlers,templates,files,vars,defaults,meta}
+    touch $role/{tasks,handlers,templates,files,vars,defaults,meta}/main.yml
+    cat > "$role/tasks/main.yml" << __EOF__
+---
+- name: sample task
+  debug:
+    msg: To be done    
+__EOF__
+    edit_file "$role/tasks/main.yml"
+}
+
+
+role_actions_menu() {
+    while true ; do
+        clear
+        title "Gestionar roles"
+        heading "Selecciona una opción"
+        echo -e "n\t- Crear nuevo rol"
+        echo -e "e\t- Editar rol"
+        echo -e "k\t- Eliminar rol"
+        echo -e "m\t- Gestionar roles externos"
+        echo -e "0\t- Volver"
+
+        read -p "Opción> " op
+
+        case $op in
+        n) ask_new_filename_then "Nombre del nuevo playbook: " "$CMDB_ANSIBLE_PATH/roles/" "" new_role ;;
+        e) choose_role_then edit_role_actions_menu ;;
+        k) choose_role_then confirm_and_do rm -rf ;;
+        m) manage_roles ;;
+        #r) choose_inventory_then run_playbook -i $inventory -p ;;        
+        0) return ;;
+        *) [ -z "$c" ] && show_message "Por favor, indica un comando." || show_error "Comando no reconocido." ;;
+        esac
+
+        [ "$?" == 0 ] || (show_error "Ocurrió un error." )
+
+        #read -p "Pulse intro para continuar." _k        
+    done        
+}
+
+environment_actions_menu() {
+    inventory=$1
+    while true ; do
+        clear
+        title "Despliegue e inventario"
+        subtitle "Entorno de despliegue: $(basename $inventory)"
+        heading "Selecciona una opción"
+        echo -e "r\t- Ejecutar playbook"
+        echo -e "i\t- Editar inventario"
+        echo -e "h\t- Variables de host"
+        echo -e "g\t- Variables de grupo de hosts"
+        #echo -e "s\t- Shell remota"
+        echo -e "0\t- Volver"
+
+        read -p "Opción> " op
+
+        case $op in
+        r) choose_playbook_then run_playbook $inventory ;;        
+        i) edit_inventory $inventory ;;
+        h) edit_host_vars $inventory ;;
+        g) edit_group_vars $inventory ;;
+        #s) remote_shell $inventory ;;
+        0) return ;;
+        *) [ -z "$c" ] && show_message "Por favor, indica un comando." || show_error "Comando no reconocido." ;;
+        esac
+
+        [ "$?" == 0 ] || (show_error "Ocurrió un error." )
+                
+    done
+}
+
+
+
+main_menu() {
+    while true ; do
+        clear
+        title "Ansible Manager"
+        # title "Entorno: $(basename $inventory)"
+        heading "Selecciona una opción"
+        echo -e "p\t- Gestionar playbooks"
+        echo -e "d\t- Despliegue e inventario"
+        #echo -e "s\t- Copiar clave SSH"
+        echo -e "r\t- Gestionar roles"
+        #echo -e "v\t- Cifrar fichero con Ansible Vault"
+        echo -e "0\t- Salir"
+        read -p "Opción> " op
+
+        case $op in
+        p) playbook_actions_menu ;;
+        d) choose_inventory_then environment_actions_menu ;;
+	    r) role_actions_menu ;;
+        #v) choose_file_then encrypt_vault_file ;;
+        0) return ;;
+        *) [ -z "$c" ] && show_message "Por favor, indica un comando." || show_error "Comando no reconocido." ;;
+        esac
+
+        [ "$?" == 0 ] || (show_error "Ocurrió un error." )
+
+        #read -p "Pulse intro para continuar." _k
         
 
     done    
